a6cce4875e7a2e027d5a802c647546746f49e450
alexFlores9 <aleexflors123@gmail.com>
2020-05-14 18:53:11 -0600

    subiendo a git

---
+++ Documents/NetBeansProjects/prueba1.1/src/prueba1/pkg1/Prueba11.java
@@ -0,0 +1,14 @@
+
+package prueba1.pkg1;
+
+
+public class Prueba11 {
+
+    
+    public static void main(String[] args) {
+        System.out.println("hola amigos");
+        
+       
+    }
+    
+}
